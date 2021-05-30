0986fed711a95eec604c9a3b99cda212843c7437
Alessandro <alessanbiz@gmail.com>
2021-05-30 19:41:57 -0300

    Indentaion errors
    erros de indentação e self.

--- funcoes/login.py
+++ funcoes/login.py
@@ -19,54 +19,32 @@ class Login():
         self.controlSend.send(
             "Seja bem vindo (a) a mais badalada sala de bate papo do Brasil. \r\nPor favor, digite seu usuario e senha \r\n\r\nLogin: ")
 
-<<<<<<< Updated upstream
-        while  self.infoUser['login'][-1:] != "\n":
-            self.infoUser['login'] = str(self.infoUser['login']) + str(conSocket.recv(1024).decode('UTF-8'))
+        while self.infoUser['login'][-1:] != "\n":
+            self.infoUser['login'] = str(
+                self.infoUser['login']) + str(conSocket.recv(1024).decode('UTF-8'))
             if(self.infoUser['login'][-1:] == "\b"):
-                  self.infoUser['login'] = str(self.infoUser['login']).replace("\b", "")
-                  self.controlSend.send("\u001b[0x08")
-     
-        self.infoUser['login'] = self.infoUser['login'].replace("\r\n", "")
-=======
-        while infoUser['login'][-1:] != "\n":
-            infoUser['login'] = str(infoUser['login']) + \
-                str(conSocket.recv(1024).decode('UTF-8'))
-            if(infoUser['login'][-1:] == "\b"):
-                infoUser['login'] = str(infoUser['login']).replace("\b", "")
+                self.infoUser['login'] = str(
+                    self.infoUser['login']).replace("\b", "")
 
-        infoUser['login'] = infoUser['login'].replace("\r\n", "")
->>>>>>> Stashed changes
+        self.infoUser['login'] = self.infoUser['login'].replace("\r\n", "")
 
         checkUser, dataJson = self.userExist(self.infoUser)
 
         # Caso o usuário exista, solicita a senha
         if(checkUser):
 
-<<<<<<< Updated upstream
-            #Solicita a senha
+            # Solicita a senha
             while self.infoUser['pass'].replace("\n", "").strip() == "":
                 self.infoUser['pass'] = ""
                 self.controlSend.send("\r\nDigite uma senha: ")
 
-                while  self.infoUser['pass'][-1:] != "\n":
-                    self.infoUser['pass'] = str(self.infoUser['pass']) + str(conSocket.recv(1024).decode('UTF-8')).replace("\b", "") 
-
-                #Realiza a comparação
-                if dataJson['pass'].strip() == self.infoUser['pass'].strip():
-                    self.infoUser['nameAlias'] = dataJson['nameAlias'].strip();
-=======
-            # Solicita a senha
-            while infoUser['pass'].replace("\n", "").strip() == "":
-                infoUser['pass'] = ""
-                self.controlSend.send("\r\nDigite uma senha: ")
-
-                while infoUser['pass'][-1:] != "\n":
-                    infoUser['pass'] = str(
-                        infoUser['pass']) + str(conSocket.recv(1024).decode('UTF-8')).replace("\b", "")
+                while self.infoUser['pass'][-1:] != "\n":
+                    self.infoUser['pass'] = str(
+                        self.infoUser['pass']) + str(conSocket.recv(1024).decode('UTF-8')).replace("\b", "")
 
                 # Realiza a comparação
-                if dataJson['pass'].strip() == infoUser['pass'].strip():
->>>>>>> Stashed changes
+                if dataJson['pass'].strip() == self.infoUser['pass'].strip():
+                    self.infoUser['nameAlias'] = dataJson['nameAlias'].strip()
                     self.loginSucesso()
                 else:
                     self.infoUser['pass'] = ""
@@ -74,54 +52,32 @@ class Login():
 
         else:
             self.controlSend.send("LOGIN NAO EXISTE!!\r\n")
-<<<<<<< Updated upstream
-            
-            #Digitar o nome do usuario para cadstro
+
+            # Digitar o nome do usuario para cadstro
             while self.infoUser['nameAlias'].replace("\n", "").strip() == "":
                 self.infoUser['nameAlias'] = ""
                 self.controlSend.send("Criar Login: \r\nNome de usuario: ")
 
-                while  self.infoUser['nameAlias'][-1:] != "\n":
-                    self.infoUser['nameAlias'] = str(self.infoUser['nameAlias']) + str(conSocket.recv(1024).decode('UTF-8')).replace("\b", "") 
+                while self.infoUser['nameAlias'][-1:] != "\n":
+                    self.infoUser['nameAlias'] = str(
+                        self.infoUser['nameAlias']) + str(conSocket.recv(1024).decode('UTF-8')).replace("\b", "")
                 print(self.infoUser['nameAlias'])
-            #Digitar a senha para cadastro do usuário
+            # Digitar a senha para cadastro do usuário
             while self.infoUser['pass'].replace("\n", "").strip() == "":
                 self.infoUser['pass'] = ""
                 self.controlSend.send("\r\nDigite uma senha: ")
 
-                while  self.infoUser['pass'][-1:] != "\n":
-                    self.infoUser['pass'] = str(self.infoUser['pass']) + str(conSocket.recv(1024).decode('UTF-8')).replace("\b", "") 
+                while self.infoUser['pass'][-1:] != "\n":
+                    self.infoUser['pass'] = str(
+                        self.infoUser['pass']) + str(conSocket.recv(1024).decode('UTF-8')).replace("\b", "")
                 print(self.infoUser['nameAlias'])
 
-                self.infoUser['nameAlias'] = self.infoUser['nameAlias'].replace("\r\n","")
-                self.infoUser['pass'] =  self.infoUser['pass'].replace("\r\n","") 
-                self.infoUser['login'] = self.infoUser['login'].replace("\r\n","") 
-=======
-
-            # Digitar o nome do usuario para cadstro
-            while infoUser['nameAlias'].replace("\n", "").strip() == "":
-                infoUser['nameAlias'] = ""
-                self.controlSend.send("Criar Login: \r\nNome de usuario: ")
-
-                while infoUser['nameAlias'][-1:] != "\n":
-                    infoUser['nameAlias'] = str(
-                        infoUser['nameAlias']) + str(conSocket.recv(1024).decode('UTF-8')).replace("\b", "")
-                print(infoUser['nameAlias'])
-            # Digitar a senha para cadastro do usuário
-            while infoUser['pass'].replace("\n", "").strip() == "":
-                infoUser['pass'] = ""
-                self.controlSend.send("\r\nDigite uma senha: ")
-
-                while infoUser['pass'][-1:] != "\n":
-                    infoUser['pass'] = str(
-                        infoUser['pass']) + str(conSocket.recv(1024).decode('UTF-8')).replace("\b", "")
-                print(infoUser['nameAlias'])
-
-                infoUser['nameAlias'] = infoUser['nameAlias'].replace(
+                self.infoUser['nameAlias'] = self.infoUser['nameAlias'].replace(
                     "\r\n", "")
-                infoUser['pass'] = infoUser['pass'].replace("\r\n", "")
-                infoUser['login'] = infoUser['login'].replace("\r\n", "")
->>>>>>> Stashed changes
+                self.infoUser['pass'] = self.infoUser['pass'].replace(
+                    "\r\n", "")
+                self.infoUser['login'] = self.infoUser['login'].replace(
+                    "\r\n", "")
 
             self.createUser(self.infoUser)
             self.controlSend.send("\r\nUsuario criado com sucesso!!!")
@@ -159,11 +115,13 @@ class Login():
         self.controlSend.send("\u001B[2J")
         print(self)
         print(self.infoUser)
-        bemVindo = "BEEEEM VINDO " + str(self.infoUser['nameAlias']).upper() + "\n"
+        bemVindo = "BEEEEM VINDO " + \
+            str(self.infoUser['nameAlias']).upper() + "\n"
         self.controlSend.send(bemVindo)
         help = batePapo.commands()
         self.controlSend.send(help)
 
         while True:
-            inputCom = batePapo.inputCommand(str(self.conSocket.recv(1024).decode('UTF-8')))
+            inputCom = batePapo.inputCommand(
+                str(self.conSocket.recv(1024).decode('UTF-8')))
             self.controlSend.send(inputCom)
